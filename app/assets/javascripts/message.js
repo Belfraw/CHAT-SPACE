0a370f7ba9f013c3fc9cc422184c35f4672b21c8
Belfraw <she-is_waiting-in-the_air@docomo.ne.jp>
2019-12-18 17:18:37 +0900

    chat-space-ajax
    chat-spaceに非同期通信を実装
    メッセージ・画像を送信できるようにする

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,67 @@
+$(function(){
+
+  function buildHTML(message){
+    // 「もしメッセージに画像が含まれていたら」という条件式
+    if (message.image) {
+      var html = `<div class="message">
+                    <div class="message__top-info">
+                      <div class="message__top-info__sender">
+                        ${message.user_name}
+                      </div>
+                      <div class="message__top-info__time">
+                        ${message.time}
+                      </div>
+                    </div>
+                  </div>
+                  <div class="message__top-info__text">
+                    ${message.content}
+                    <img src="${message.image}">
+                  </div>`;//メッセージに画像が含まれる場合のHTMLを作る
+    } else {
+      var html =  `<div class="message">
+                    <div class="message__top-info">
+                      <div class="message__top-info__sender">
+                        ${message.user_name}
+                      </div>
+                      <div class="message__top-info__time">
+                        ${message.time}
+                      </div>
+                    </div>
+                  </div>
+                  <div class="message__top-info__text">
+                    ${message.content}
+                  </div>`;//メッセージに画像が含まれない場合のHTMLを作る
+    }
+    return html
+  }
+
+function scroll() {
+  $('.main__chat__message__board').animate({ scrollTop: $('.main__chat__message__board')[0].scrollHeight});
+}
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,  //同期通信でいう『パス』
+      type: 'POST',  //同期通信でいう『HTTPメソッド』
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      console.log(data.image);
+      var html = buildHTML(data);     
+      $('.main__chat__message__board').append(html);
+      $('#new_message')[0].reset()
+      $('.text__send-btn').prop('disabled', false);
+      scroll()
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+      $('.text__send-btn').prop('disabled', false);
+  })
+  })
+});
