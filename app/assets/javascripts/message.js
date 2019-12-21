678fabcdf20968a17bbf71cbf7ccdfb746ee8738
Belfraw <she-is_waiting-in-the_air@docomo.ne.jp>
2019-12-21 10:37:50 +0900

    自動更新の設定
    # what
    chat-spaceに自動更新を設定する
    
    # why
    グループチャットアプリなので、相手PC画面にもリロードなしで、新しいメッセージを表示させたい。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,17 @@
 $(function(){
 
-  function buildHTML(message){
+ 
+
+  var buildHTML = function (message){
     // 「もしメッセージに画像が含まれていたら」という条件式
-    if (message.image) {
-      var html = `<div class="message">
+    if (message.content && message.image) {
+      var html = `<div class="message" data-message-id="${message.id}">
                     <div class="message__top-info">
                       <div class="message__top-info__sender">
                         ${message.user_name}
                       </div>
                       <div class="message__top-info__time">
-                        ${message.time}
+                        ${message.created_at}
                       </div>
                     </div>
                   </div>
@@ -17,27 +19,41 @@ $(function(){
                     ${message.content}
                     <img src="${message.image}">
                   </div>`;//メッセージに画像が含まれる場合のHTMLを作る
-    } else {
-      var html =  `<div class="message">
+    } else if (message.content) {
+      var html = `<div class="message" data-message-id="${message.id}">
                     <div class="message__top-info">
                       <div class="message__top-info__sender">
                         ${message.user_name}
                       </div>
                       <div class="message__top-info__time">
-                        ${message.time}
+                        ${message.created_at}
                       </div>
                     </div>
                   </div>
                   <div class="message__top-info__text">
                     ${message.content}
                   </div>`;//メッセージに画像が含まれない場合のHTMLを作る
-    }
-    return html
-  }
+    } else if (message.image) {
+      var html = `<div class="message" data-message-id="${message.id}">
+                    <div class="message__top-info">
+                      <div class="message__top-info__sender">
+                        ${message.user_name}
+                      </div>
+                      <div class="message__top-info__time">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                  </div>
+                  <div class="message__top-info__text">
+                    <img src="${message.image}">
+                  </div>`;
+    };
+    return html;
+  };
 
-function scroll() {
-  $('.main__chat__message__board').animate({ scrollTop: $('.main__chat__message__board')[0].scrollHeight});
-}
+    function scroll() {
+      $('.main__chat__message__board').animate({ scrollTop: $('.main__chat__message__board')[0].scrollHeight});
+    }
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -61,6 +77,35 @@ function scroll() {
     .fail(function() {
       alert("メッセージ送信に失敗しました");
       $('.text__send-btn').prop('disabled', false);
+    })
   })
-  })
+
+    var reloadMessages = function(){
+      if (window.location.href.match(/\/groups\/\d+\/messages/)){    // group/:group_id/messagesというURLの時だけ、以降の記述が実行されます。
+      var href = 'api/messages#index {:format=>"json"}'              // リクエスト先と形式を指定しています。
+      var last_message_id = $('.message:last').data('message-id');   // カスタムデータ属性を利用して、最新のメッセージIDを取得しています。
+
+      // ajaxの形式をそれぞれ指定しています。
+      $.ajax({
+        url:  href,
+        type: 'GET',
+        data: {id: last_message_id},
+        dataType: 'json'
+      })
+
+
+      .done(function(messages){        // フォームに入力されたデータを引数として取得しています。
+        var insertHTML='';
+          messages.forEach(function(message){
+            insertHTML = buildHTML(message);
+            $('.main__chat__message__board').append(insertHTML);
+            $('.main__chat__message__board').animate({ scrollTop: $('.main__chat__message__board')[0].scrollHeight});
+          });
+      })
+      .fail(function(){
+        alert("自動更新に失敗しました")
+      });
+    };
+  };
+  setInterval(reloadMessages, 7000);
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,5 @@
 json.content @message.content
 json.id @message.id
 json.user_name @message.user.name
-json.time @message.created_at
 json.image @message.image.url
+json.created_at @message.created_at.strftime("%Y年%m月%d日 %H時%M分")
